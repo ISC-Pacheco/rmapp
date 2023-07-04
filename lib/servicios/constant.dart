f9a70b9970fb77768ead84f0f6a38468d3a21aa8
iscpa <iscpacheco17@gmail.com>
2023-07-03 19:11:11 -0600

    Conectando a server

--- lib/servicios/constant.dart
+++ lib/servicios/constant.dart
@@ -6,13 +6,13 @@ Agregar las rutas de las peticiones que se van a realizar al servidor
  */
 class APIconstant {
   //IP o url del servidor
-  static String base_URL = "http://192.168.1.103";
+  static String base_URL = "https://bewwednethost.000webhostapp.com";
   //peticion login
-  static String rutaLogin = "/tesvb/login.php";
+  static String rutaLogin = "/rmapp/bienes/login.php";
   //peticion get bienes materiales
-  static String rutagetbienes = "/tesvb/GET_ALL.php";
+  static String rutagetbienes = "/rmapp/bienes/GET_ALL.php";
   //peticion reporte de bienes
-  static String rutareportebienes = "/tesvb/GET_ALL.php";
+  static String rutareportebienes = "/rmapp/bienes/GET_ALL.php";
   //peticion busqueda de bienes
   static String rutaSearch = "/tesvb/GET_NICK.php?nick=";
   //peticion para facturas
@@ -20,7 +20,7 @@ class APIconstant {
   //peticion para asociados
   static String rutaAsociados = "/tesvb/asociados.php";
   //peticion para resguardos
-  static String rutaResguardos = "/tesvb/GET_RESGUARDOS.php";
+  static String rutaResguardos = "/rmapp/bienes/GET_RESGUARDOS.php";
   //peticion para cambiar estados
-  static String rutaCambiarEstado = "/tesvb/UPDATE_ESTADO.php";
+  static String rutaCambiarEstado = "/rmapp/bienes/UPDATE_ESTADO.php";
 }
